c498ccd4816e313d8776ad4d5b418b2719be7242
CharlesVall <Charlessenardvallois@gmail.com>
2026-02-19 18:20:49 +0100

    style: header, cv-download, work-card update

--- src/app/landing/contact/footer-board/download-button/download-button.ts
+++ src/app/landing/contact/footer-board/download-button/download-button.ts
@@ -10,11 +10,4 @@ import { MagicResizeService } from '@core/services/magic-resize-service/magic-re
 export class DownloadButton {
   private readonly magicResizeService = inject(MagicResizeService)
   protected readonly magicColor = this.magicResizeService.magicColor;
-
-  protected downloadCV(): void {
-    const link = document.createElement('a');
-    link.href = 'files/CV-Sénard--Vallois.pdf';
-    link.download = 'CV-Sénard--Vallois.pdf';
-    link.click();
-  }
 }
